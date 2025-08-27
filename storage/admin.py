a8ddc24468293778c67f536534bed241bd0828ae
Chepik Pavel <126232792+Ou7ro@users.noreply.github.com>
2025-08-28 01:10:01 +0500

    Add cake, order and courier models with admin (#1)
    * Add cake, order and courier models with admin

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Courier, Order
+
+
+@admin.register(Courier)
+class CourierAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'rating']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address', 'order_notes']
+    list_filter = ['delivery_date', 'courier']
+    readonly_fields = ['created_at']
+
+    fieldsets = (
+        ('Информация о заказе', {
+            'fields': ('user', 'cake', 'courier', 'cost'),
+            'description': 'Основные данные заказа'
+        }),
+        ('Доставка', {
+            'fields': ('address', 'delivery_date', 'delivery_time'),
+            'description': 'Информация о доставке'
+        }),
+        ('Примечания', {
+            'fields': ('order_notes', 'delivery_notes'),
+            'classes': ('collapse',),
+            'description': 'Дополнительные заметки к заказу'
+        }),
+        ('Системная информация', {
+            'fields': ('created_at',),
+            'classes': ('collapse',),
+            'description': 'Техническая информация'
+        }),
+    )
